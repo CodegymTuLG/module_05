2bd3756b6c54bcd613be9b6c9376ab2b70967c03
tulg <tulg@mcrew-tech.com>
2023-04-12 20:20:57 +0700

    ss4 done

--- module5/src/app/app.module.ts
+++ module5/src/app/app.module.ts
@@ -5,15 +5,26 @@ import { AppComponent } from './app.component';
 import { Ss3Component } from './ss3/ss3.component';
 import { B1calculatorComponent } from './ss3/b1calculator/b1calculator.component';
 import { B2colorComponent } from './ss3/b2color/b2color.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule} from '@angular/forms';
+import { Ss4Component } from './ss4/ss4.component';
+import {ArticleComponent} from './ss4/article/article.component';
+import { LikeComponent } from './ss4/like/like.component';
+import { NavbarComponent } from './ss4/navbar/navbar.component';
+import { FooterComponent } from './ss4/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    Ss3Component,
-    B1calculatorComponent,
-    B2colorComponent
-  ],
+    declarations: [
+        AppComponent,
+        Ss3Component,
+        B1calculatorComponent,
+        B2colorComponent,
+        Ss4Component,
+        ArticleComponent,
+        ArticleComponent,
+        LikeComponent,
+        NavbarComponent,
+        FooterComponent
+    ],
     imports: [
         BrowserModule,
         FormsModule
---
+++ module5/src/app/ss4/ss4.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ss4',
+  templateUrl: './ss4.component.html',
+  styleUrls: ['./ss4.component.css']
+})
+export class Ss4Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
