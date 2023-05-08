4124381738ab7ec9125c5aec53f7657e4de7e73d
tulg <tulg@mcrew-tech.com>
2023-05-08 20:48:21 +0700

    update

--- module5/src/app/app-routing.module.ts
+++ module5/src/app/app-routing.module.ts
@@ -3,7 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import {DictionaryDetailComponentComponent} from './ss7/bt1/component/dictionary-detail-component/dictionary-detail-component.component';
 
 const routes: Routes = [
-  { path: 'detail', component: DictionaryDetailComponentComponent}
+  { path: 'detail', component: DictionaryDetailComponentComponent},
+  {
+    path: 'product',
+    loadChildren: () => import('./ss7/bt2/product/product.module').then(module => module.ProductModule)
+  },
+  {
+    path: 'category',
+    loadChildren: () => import('./ss7/bt2/category/category.module').then(module => module.CategoryModule)
+  }
 ];
 
 @NgModule({
